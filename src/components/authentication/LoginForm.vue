1a133a41a7ebe4b3dc07d28a1f810ddb2cfef17e
Febrian Andi Nugroho <febrianandinugroho@gmail.com>
2023-10-09 14:18:54 +0700

    add notice if email or password is incorrect

--- src/components/authentication/LoginForm.vue
+++ src/components/authentication/LoginForm.vue
@@ -10,50 +10,59 @@ const router = useRouter();
 const form = ref({
   email: '',
   password: '',
-})
+});
+
+const loginError = ref('');
 
 async function login() {
   try {
     const response = await axios.post('https://zullkit-backend.demo.belajarkoding.com/api/login', {
-            email: form.value.email,
-            password: form.value.password,
-        }
+      email: form.value.email,
+      password: form.value.password,
+    }
     );
     console.log(response.data);
-    
+
     localStorage.setItem('access_token', response.data.data.access_token);
     localStorage.setItem('token_type', response.data.data.token_type);
-    
+
     userStore.fetchUser();
+    loginError.value = '';
     router.push("/");
 
   } catch (error) {
+    loginError.value = 'Email or Password is incorrect';
     console.log(error);
   }
-}
+};
 </script>
 
 <template>
-    <form>
-        <div class="mb-4">
-            <label class="block mb-1" for="email">Email Address</label>
-            <input v-model="form.email" placeholder="Type your email" id="email" type="text" name="email" autocomplete="username"
-                class="block w-full py-3 mt-2 border border-gray-300 rounded-full shadow-sm px-7 focus:border-indigo-300 focus:outline-none focus:ring focus:ring-indigo-200 focus:ring-opacity-50 disabled:bg-gray-100" />
-        </div>
-        <div class="mb-4">
-            <label class="block mb-1" for="password">Password</label>
-            <input @keyup.enter="login" v-model="form.password" placeholder="Type your password" id="password" type="password" name="password" autocomplete="current-password"
-                class="block w-full py-3 mt-2 border border-gray-300 rounded-full shadow-sm px-7 focus:border-indigo-300 focus:outline-none focus:ring focus:ring-indigo-200 focus:ring-opacity-50 disabled:bg-gray-100" />
-        </div>
-        <div class="mt-6">
-            <button @click="login" type="button"
-                class="inline-flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-full hover:bg-indigo-700 md:py-2 md:text-lg md:px-10 hover:shadow">
-                Sign In
-            </button>
-            <RouterLink to="/register"
-                class="inline-flex items-center justify-center w-full px-8 py-3 mt-2 text-base font-medium text-black bg-gray-200 border border-transparent rounded-full hover:bg-gray-300 md:py-2 md:text-lg md:px-10 hover:shadow">
-                Create New Account
-            </RouterLink>
-        </div>
-    </form>
+  <form>
+    <div v-if="loginError" class="bg-red-200 bg-opacity-30 border-0 text-red-500 rounded-lg p-2 mb-3">
+      {{ loginError }}
+    </div>
+    <div class="mb-4">
+      <label class="block mb-1" for="email">Email Address</label>
+      <input v-model="form.email" placeholder="Type your email" id="email" type="text" name="email"
+        autocomplete="username"
+        class="block w-full py-3 mt-2 border border-gray-300 rounded-full shadow-sm px-7 focus:border-indigo-300 focus:outline-none focus:ring focus:ring-indigo-200 focus:ring-opacity-50 disabled:bg-gray-100" />
+    </div>
+    <div class="mb-4">
+      <label class="block mb-1" for="password">Password</label>
+      <input @keyup.enter="login" v-model="form.password" placeholder="Type your password" id="password" type="password"
+        name="password" autocomplete="current-password"
+        class="block w-full py-3 mt-2 border border-gray-300 rounded-full shadow-sm px-7 focus:border-indigo-300 focus:outline-none focus:ring focus:ring-indigo-200 focus:ring-opacity-50 disabled:bg-gray-100" />
+    </div>
+    <div class="mt-6">
+      <button @click="login" type="button"
+        class="inline-flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-full hover:bg-indigo-700 md:py-2 md:text-lg md:px-10 hover:shadow">
+        Sign In
+      </button>
+      <RouterLink to="/register"
+        class="inline-flex items-center justify-center w-full px-8 py-3 mt-2 text-base font-medium text-black bg-gray-200 border border-transparent rounded-full hover:bg-gray-300 md:py-2 md:text-lg md:px-10 hover:shadow">
+        Create New Account
+      </RouterLink>
+    </div>
+  </form>
 </template>
